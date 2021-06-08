acba3794d3fd8e9f20a5c71d73aed8fcb804932c
Harmo00ny <iseekana@gmail.com>
2021-06-08 13:49:17 +0900

    Implement display word processing

--- app/src/main/java/com/marysugar/room_sample_java/MainActivity.java
+++ app/src/main/java/com/marysugar/room_sample_java/MainActivity.java
@@ -1,6 +1,7 @@
 package com.marysugar.room_sample_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,13 +10,23 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+    private WordViewModel mWordViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         RecyclerView recyclerView = findViewById(R.id.rv);
         final WordListAdapter adapter = new WordListAdapter(new WordListAdapter.WordDiff());
         recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mWordViewModel = new ViewModelProvider(
+                this,
+                new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(WordViewModel.class);
+
+        mWordViewModel.getAllWords().observe(this, adapter::submitList);
     }
 }
