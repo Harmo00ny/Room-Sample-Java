ea4548f250a2b8e0d1303033a941237710fd9028
Harmo00ny <iseekana@gmail.com>
2021-06-08 12:36:33 +0900

    Implement RecyclerViewAdapter

--- app/src/main/java/com/marysugar/room_sample_java/MainActivity.java
+++ app/src/main/java/com/marysugar/room_sample_java/MainActivity.java
@@ -1,6 +1,9 @@
 package com.marysugar.room_sample_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.rv);
+        final WordListAdapter adapter = new WordListAdapter(new WordListAdapter.WordDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
     }
 }
